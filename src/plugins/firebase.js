dd99cf5f48af9ccb2bbaf84cb6051699a89071c5
Azad Furkan ŞAKAR <afsakarr@gmail.com>
2023-01-14 18:53:08 +0300

    firebase config clear

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -4,12 +4,12 @@ import {getFirestore} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebase = initializeApp({
-    apiKey: "AIzaSyApmNbf6I2JtjvXcgMeOntuwzY1eB2Gp74",
-    authDomain: "vuebase-724e6.firebaseapp.com",
-    projectId: "vuebase-724e6",
-    storageBucket: "vuebase-724e6.appspot.com",
-    messagingSenderId: "97512455113",
-    appId: "1:97512455113:web:25bac76fc4d404a8650618",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 });
 
 const db = getFirestore(firebase);
